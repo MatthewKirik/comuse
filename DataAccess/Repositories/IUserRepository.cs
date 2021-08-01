2ba34dfa1d27adb48ce7432ac9a85a259576582c
Матвей Кирик <ikirik59@gmail.com>
2021-08-02 00:50:25 +0300

    Add User Repository

---
+++ DataAccess/Repositories/IUserRepository.cs
@@ -0,0 +1,15 @@
+﻿using DataTransfer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Repositories
+{
+    public interface IUserRepository
+    {
+        Task<UserDTO> AddUser(UserDTO user, string password);
+        Task<UserDTO> GetUser(int id);
+    }
+}
